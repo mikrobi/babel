c0e096c95eb6ad445a70ff1b405c0726c3162e6f
Romain <romain@meltingmedia.net>
2010-12-16 16:36:19 +0100

    fast french translation

---
+++ core/components/babel/lexicon/fr/default.inc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Babel French language file
+ * 
+ * @author Jakob Class <jakob.class@class-zec.de>
+ *
+ * @package babel
+ * @subpackage lexicon
+ * 
+ * @todo complete babel.language_xx entries for every language
+ */
+
+$_lang['babel.tv_caption'] = 'Liens de traduction de Babel';
+$_lang['babel.tv_description'] = 'Mis à jour par le plugin Babel. Veuillez ne pas modifier!';
+$_lang['babel.create_translations'] = 'Créer une traduction';
+$_lang['babel.translation_pending'] = '[traduction en attente]';
+
+/* language names */
+$_lang['babel.language_de'] = 'Allemand';
+$_lang['babel.language_en'] = 'Anglais';
+$_lang['babel.language_es'] = 'Espagnol';
+$_lang['babel.language_fr'] = 'Français';
+$_lang['babel.language_nl'] = 'Hollandais';
+$_lang['babel.language_ru'] = 'Russe';
---
+++ core/components/babel/lexicon/fr/properties.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Babel properties French language file
+ * 
+ * @author Jakob Class <jakob.class@class-zec.de>
+ *
+ * @package babel
+ * @subpackage lexicon
+ */
+
+$_lang['babel.tpl_desc'] = 'Chunk pour afficher un lien de langue';
+$_lang['babel.activeCls_desc'] = 'Classe CSS pour les liens de la langue active';
