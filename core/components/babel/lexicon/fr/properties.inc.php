8e61aeb577072ed81b556416f8fdcbfd569c4ed2
Thomas Jakobi <buero@treehillstudio.de>
2024-01-17 15:05:41 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: MODX Extras/Properties
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/babel/properties/fr/

--- core/components/babel/lexicon/fr/properties.inc.php
+++ core/components/babel/lexicon/fr/properties.inc.php
@@ -5,17 +5,17 @@
  * @package babel
  * @subpackage lexicon
  */
-$_lang['babel.babellinks.activeCls'] = 'Classe CSS pour les liens de la langue active';
+$_lang['babel.babellinks.activeCls'] = 'Classe CSS pour les liens de la langue active.';
 $_lang['babel.babellinks.ignoreSiteStatus'] = 'Drapeau indiquant s\'il faut ignorer le site_status (lorsque le site est hors ligne)';
 $_lang['babel.babellinks.includeUnlinked'] = 'Indique s\'il faut afficher le contexte non lié.';
-$_lang['babel.babellinks.resourceId'] = 'Id de la ressource dont les liens vers les traductions doivent être affichés';
-$_lang['babel.babellinks.showCurrent'] = 'Indicateur permettant d\'afficher ou non un lien vers une traduction de la langue actuelle';
-$_lang['babel.babellinks.showUnpublished'] = 'Indique s\'il faut afficher les traductions non publiées';
-$_lang['babel.babellinks.toArray'] = 'Indique s\'il faut afficher la sortie sous forme de tableau';
-$_lang['babel.babellinks.toPlaceholder'] = 'Indique s\'il faut afficher le résultat dans le nom du placeholder donné';
-$_lang['babel.babellinks.tpl'] = 'Chunk pour afficher un lien de langue';
+$_lang['babel.babellinks.resourceId'] = 'Id de la ressource dont les liens vers les traductions doivent être affichés.';
+$_lang['babel.babellinks.showCurrent'] = 'Indicateur permettant d\'afficher ou non un lien vers une traduction de la langue actuelle.';
+$_lang['babel.babellinks.showUnpublished'] = 'Indique s\'il faut afficher les traductions non publiées.';
+$_lang['babel.babellinks.toArray'] = 'Indique s\'il faut afficher la sortie sous forme de tableau.';
+$_lang['babel.babellinks.toPlaceholder'] = 'Indique s\'il faut afficher le résultat dans le nom du placeholder donné.';
+$_lang['babel.babellinks.tpl'] = 'Chunk pour afficher un lien de langue.';
 $_lang['babel.babellinks.wrapperTpl'] = 'Morceau permettant de regrouper tous les liens linguistiques. L\'espace réservé pour tous les liens est [[+babelLinks]].';
-$_lang['babel.babeltranslation.contextKey'] = 'Clé du contexte dans lequel la ressource traduite doit être déterminée';
+$_lang['babel.babeltranslation.contextKey'] = 'Clé du contexte dans lequel la ressource traduite doit être déterminée.';
 $_lang['babel.babeltranslation.cultureKey'] = 'Clé de la culture dans laquelle la ressource traduite doit être déterminée. Utilisée uniquement dans le cas où contextKey n\'a pas été spécifié. Si les deux sont omis : utilise la cultureKey actuellement définie.';
-$_lang['babel.babeltranslation.resourceId'] = 'ID de la ressource dont la ressource traduite doit être déterminée';
-$_lang['babel.babeltranslation.showUnpublished'] = 'Indiquer s\'il faut afficher les traductions non publiées';
+$_lang['babel.babeltranslation.resourceId'] = 'ID de la ressource dont la ressource traduite doit être déterminée.';
+$_lang['babel.babeltranslation.showUnpublished'] = 'Indiquer s\'il faut afficher les traductions non publiées.';
