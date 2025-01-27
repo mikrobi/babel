3e59a58de615c9cc76174c2e32e6e71e56de19ff
LexOn937 <info@dnrdev.ru>
2025-01-27 17:22:27 +0300

    fix updatefromgrid path to processor

--- core/components/babel/src/Processors/Context/Setting/V2/UpdateFromGrid.php
+++ core/components/babel/src/Processors/Context/Setting/V2/UpdateFromGrid.php
@@ -2,7 +2,7 @@
 
 namespace mikrobi\Babel\Processors\Context\Setting\V2;
 
-require_once MODX_CORE_PATH . 'model/modx/processors/context/setting/updatefromgid.class.php';
+require_once MODX_CORE_PATH . 'model/modx/processors/context/setting/updatefromgrid.class.php';
 
 class UpdateFromGrid extends \modContextSettingUpdateFromGridProcessor
 {
