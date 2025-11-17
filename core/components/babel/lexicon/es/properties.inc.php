585f6482c054ccff4c6460ff37c41d10acacdcc0
Thomas Jakobi <thomas.jakobi@partout.info>
2025-11-17 21:31:59 +0100

    - New spanish languages lexicon

---
+++ core/components/babel/lexicon/es/properties.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Properties Lexicon Entries for Babel
+ *
+ * @package babel
+ * @subpackage lexicon
+ */
+$_lang['babel.babellinks.activeCls'] = 'Clase CSS para el enlace de idioma activo.';
+$_lang['babel.babellinks.ignoreSiteStatus'] = 'Marca si se debe ignorar el site_status (cuando el sitio está desconectado)';
+$_lang['babel.babellinks.includeUnlinked'] = 'Marca si se muestra el contexto no enlazado.';
+$_lang['babel.babellinks.resourceId'] = 'ID del recurso cuyos enlaces a traducciones deben mostrarse.';
+$_lang['babel.babellinks.restrictToGroup'] = 'Restringe los contextos de la salida del fragmento al grupo del contexto actual.';
+$_lang['babel.babellinks.showCurrent'] = 'Marca si se debe mostrar un enlace a una traducción del idioma actual.';
+$_lang['babel.babellinks.showUnpublished'] = 'Marca si se muestran las traducciones no publicadas.';
+$_lang['babel.babellinks.sortby'] = 'Campo de contexto por el que ordenar los resultados. El valor predeterminado \'babel\' significa ordenar por el orden de las claves de contexto en la configuración del sistema babel.contextKeys.';
+$_lang['babel.babellinks.sortdir'] = 'La dirección a ordenar.';
+$_lang['babel.babellinks.toArray'] = 'Marca si se debe volcar la salida como una matriz en su lugar.';
+$_lang['babel.babellinks.toPlaceholder'] = 'Marca si se debe volcar la salida en el nombre del marcador de posición dado.';
+$_lang['babel.babellinks.tpl'] = 'Chunk para mostrar un enlace de idioma.';
+$_lang['babel.babellinks.wrapperTpl'] = 'Chunk para envolver todos los enlaces de idioma. El marcador de posición para todos los enlaces es [[+babelLinks]].';
+$_lang['babel.babeltranslation.contextKey'] = 'Clave del contexto en el que debe determinarse el recurso traducido.';
+$_lang['babel.babeltranslation.cultureKey'] = 'Clave de la cultura en la que debe determinarse el recurso traducido. Sólo se utiliza en caso de que no se haya especificado contextKey. Si ambos omitieron el fragmento el cultureKey establecido actualmente.';
+$_lang['babel.babeltranslation.resourceId'] = 'Lista separada por comas de ID de recursos de los que debe determinarse una lista separada por comas de recursos traducidos en el contextKey/cultureKey.';
+$_lang['babel.babeltranslation.showUnpublished'] = 'Marca si se muestran las traducciones no publicadas.';
+$_lang['babel.babeltranslation.toPlaceholder'] = 'Marca si se debe volcar la salida en el nombre del marcador de posición dado.';
