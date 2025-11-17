3b57b3e45675771f3da4b1c5beeb5cfcde2e818d
Thomas Jakobi <thomas.jakobi@partout.info>
2025-11-17 21:03:39 +0100

    Fix english properties lexicon

--- core/components/babel/lexicon/en/properties.inc.php
+++ core/components/babel/lexicon/en/properties.inc.php
@@ -6,20 +6,21 @@
  * @subpackage lexicon
  */
 $_lang['babel.babellinks.activeCls'] = 'CSS class for the active language link.';
-$_lang['babel.babellinks.ignoreSiteStatus'] = 'Flag whether to ignore the site_status (when the site is offline)';
-$_lang['babel.babellinks.includeUnlinked'] = 'Flag whether to show unlinked context.';
+$_lang['babel.babellinks.ignoreSiteStatus'] = 'Specifies whether the site_status is ignored (when the site is offline)';
+$_lang['babel.babellinks.includeUnlinked'] = 'Specifies whether the unlinked context are shown.';
 $_lang['babel.babellinks.resourceId'] = 'ID of resource of which links to translations should be displayed.';
 $_lang['babel.babellinks.restrictToGroup'] = 'Restrict the contexts in the snippet output to the group of the current context.';
-$_lang['babel.babellinks.showCurrent'] = 'Flag whether to show a link to a translation of the current language.';
-$_lang['babel.babellinks.showUnpublished'] = 'Flag whether to show unpublished translations.';
+$_lang['babel.babellinks.showCurrent'] = 'Specifies whether a link to the current language is shown.';
+$_lang['babel.babellinks.showUnpublished'] = 'Specifies whether the unpublished translations are shown.';
 $_lang['babel.babellinks.sortby'] = 'Context field to sort the results by. The default \'babel\' means to sort by the order of the context keys in the babel.contextKeys system setting.';
 $_lang['babel.babellinks.sortdir'] = 'The direction to sort in.';
-$_lang['babel.babellinks.toArray'] = 'Flag whether to dump the output as an array instead.';
-$_lang['babel.babellinks.toPlaceholder'] = 'Flag whether to dump the output into the given placeholder\'s name.';
+$_lang['babel.babellinks.toArray'] = 'Specifies whether the output is dumped as an array.';
+$_lang['babel.babellinks.toPlaceholder'] = 'Specifies whether the output is dumped into the given placeholder\'s name.';
 $_lang['babel.babellinks.tpl'] = 'Chunk to display a language link.';
+$_lang['babel.babellinks.useRequestProperties'] = 'Specifies whether the current request properties are used in the links to the translated resources.';
 $_lang['babel.babellinks.wrapperTpl'] = 'Chunk to wrap all language links. The placeholder for all links is [[+babelLinks]].';
 $_lang['babel.babeltranslation.contextKey'] = 'Key of context in which translated resource should be determined.';
 $_lang['babel.babeltranslation.cultureKey'] = 'Key of culture in which translated resource should be determined. Used only in case contextKey was not specified. If both omitted the snippet the currently set cultureKey.';
 $_lang['babel.babeltranslation.resourceId'] = 'Comma separated list of resource IDs of which a comma separated list of translated resources in the contextKey/cultureKey should be determined.';
-$_lang['babel.babeltranslation.showUnpublished'] = 'Flag whether to show unpublished translations.';
-$_lang['babel.babeltranslation.toPlaceholder'] = 'Flag whether to dump the output into the given placeholder\'s name.';
+$_lang['babel.babeltranslation.showUnpublished'] = 'Specifies whether the unpublished translations are shown.';
+$_lang['babel.babeltranslation.toPlaceholder'] = 'Specifies whether to dump the output into the given placeholder\'s name.';
